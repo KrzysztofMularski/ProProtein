165e02423ddb337b2e513d7ba17a876e252928c7
KrzysztofMularski <krzysztof.mularski@student.put.poznan.pl>
2022-04-24 22:18:28 +0200

    adding admin page

--- server/app/src/db/models/user.js
+++ server/app/src/db/models/user.js
@@ -24,6 +24,10 @@ const userSchema = new mongoose.Schema({
     created: {
         type: Date,
         default: Date.now
+    },
+    isAdmin: {
+        type: String,
+        default: false
     }
 })
 
